3b979c7a47013210a09d6ffdaabaabfdda0b1b03
quangnguyen <>
2025-04-11 00:32:55 +0700

    Init project

---
+++ front_end/data/personalInfo.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { meta: "first name", metaInfo: "Quang" },
+  { meta: "last name", metaInfo: "Nguyen" },
+  { meta: "Age", metaInfo: "27 Years" },
+  { meta: "Nationality", metaInfo: "Viet Nam" },
+  { meta: "Freelance", metaInfo: "Available" },
+  { meta: "Address", metaInfo: "Hue" },
+  { meta: "phone", metaInfo: "+84347811798" },
+  { meta: "Email", metaInfo: "quangnd.nta@mail.com" },
+  { meta: "Skype", metaInfo: " rokers.nelson" },
+  { meta: "langages", metaInfo: "English" },
+];
